5590db8b3e364cac5569c159291889d5d13bd9bd
danirus6 <44194174+danirus6@users.noreply.github.com>
2023-10-06 20:07:38 +0200

    Co-authored-by: LauraGarciaa <LauraGarciaa@users.noreply.github.com> Co-authored-by: EderJulian <EderJulian@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,65 @@
+/*
+//1
+const Coche = {
+    marca: "Audi",
+    modelo: "A1",
+    matricula: "UwU"
+};
+//2
+const Casa = {
+    codPostal: "11111",
+    calle: "Rue del percebe",
+    portal: "2",
+    piso: "Izquierda"
+}; 
+//3
+const FullStackDeveloper = {
+    lenguajes: [],
+    proyectos: []
+};
+//4
+const Perro = {
+    nombre: "Chibi",
+    raza: "chihuahua",
+    color: "blanco",
+    edad: 33,
+    ladrar: function () {
+        return "GUAU";
+    },
+    popo: function () {
+        return Math.random() *3;
+    }
+};
+//5
+const Portatil = {
+    marca: "Lenovo"
+};
+const marcaPortatil = Portatil.marca;
+//6
+const Portatil2 = {
+    marca: "Lenovo2"
+};
+const marcaPortatil1 = Portatil2["marca"];
+//7
+const Concierto = {
+    grupos: ["grupo1","grupo2"]
+};
+const grupos = Concierto["grupos"];*/
+//8
+const Led = {
+    rojo: false,
+    verde: false,
+    azul: true,
+};
+const arrayRGB = [Led.rojo, Led.verde, Led.azul]
+//9
+/*
+const Portatil3 = {
+    modelo: "nombre"
+};
+*/
+//Eder
+
+
+//Laura
+//Portatil3.modelo = "P345";
